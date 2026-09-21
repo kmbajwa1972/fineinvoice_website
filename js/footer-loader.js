bde9be4c0f11b8e8c23c65b50bac4db54d522d8c
kmbajwa1972 <kmbajwa1972@gmail.com>
2026-09-21 02:52:32 -0700

    Support footer placeholders on public pages

--- js/footer-loader.js
+++ js/footer-loader.js
@@ -2,9 +2,10 @@
 fetch("footer.html")
   .then(response => response.text())
   .then(html => {
-    const footer = document.querySelector("footer");
-    if (footer) {
-      footer.outerHTML = html;
+    const target = document.querySelector("#site-footer") || document.querySelector("footer");
+    if (target) {
+      if (target.id === "site-footer") target.innerHTML = html;
+      else target.outerHTML = html;
     }
   })
   .catch(() => {});
